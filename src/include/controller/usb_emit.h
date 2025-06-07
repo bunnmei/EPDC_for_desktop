b64c3a2572908abd95f00dd231f4e74734f3959e
bunnmei <kombinat.web@gmail.com>
2025-06-07 23:43:36 +0900

    usb 配列の送信確認

--- src/include/controller/usb_emit.h
+++ src/include/controller/usb_emit.h
@@ -4,4 +4,7 @@
 #include <gtk/gtk.h>
 #include "object_common.h"
 
+extern unsigned char gImage1_BW[4736]; // 宣言のみ
+extern unsigned char gImage1_RED[4736]; // 宣言のみ
+
 bool usb_emit(GPtrArray *array, PopUp *pop_up);
